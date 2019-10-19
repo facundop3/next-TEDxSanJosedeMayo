58dd2405a473c429325609e2731ab56b4282bd5f
Facundo Petre <facu1p3@gmail.com>
2019-10-19 09:24:42 -0300

    fix[year]js

--- pages/talks/[year].js
+++ pages/talks/[year].js
@@ -1,10 +1,8 @@
-import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
 import Head from "next/head";
 import fetch from "isomorphic-unfetch";
 const Talk = props => {
-  const router = useRouter();
-  const { year } = router.query;
+  const { year } = props;
   return (
     <Layout>
       <Head>
@@ -12,15 +10,16 @@ const Talk = props => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>TedxSanJosedeMayo {year} </h1>
-      <p>{props.speakers[year][0].summary}</p>
+      <p>{props.speakers[0].summary}</p>
     </Layout>
   );
 };
 
-Talk.getInitialProps = async () => {
-  const reqSpeakers = await fetch("http://localhost:3001/speakers");
-  const speakers = await reqSpeakers.json();
-  return { speakers };
+Talk.getInitialProps = async ({ query }) => {
+  const { year } = query;
+  const reqSpeakers = await fetch(`http://localhost:3001/speakers/${year}`);
+  const { data: speakers } = await reqSpeakers.json();
+  return { speakers, year };
 };
 
 export default Talk;
